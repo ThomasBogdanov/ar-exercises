607b0a125473fa499014dcdfebbe66151b221535
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-10-17 03:03:50 +0000

    fixed gem issue + exercise 1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Burnaby = Store.create do |s|
+  s.name = "Burnaby"
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+Richmond = Store.create do |s|
+  s.name = "Richmond"
+  s.annual_revenue = 126000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+Gastown = Store.create do |s|
+  s.name = "Gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+stores = Store.all
+puts stores.count
