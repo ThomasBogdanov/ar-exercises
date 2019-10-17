9f2816fde0c3b711f4eeacc9fc6af643676a7838
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-10-17 04:44:44 +0000

    updated class models and completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tom", last_name: "B", hourly_rate: 0)
+@store1.employees.create(first_name: "Employee1", last_name: "Emp1", hourly_rate: 10000)
+
+@store2.employees.create(first_name: "Emp2", last_name: "222", hourly_rate: 35)
+@store2.employees.create(first_name: "Emp3", last_name: "333", hourly_rate: 20)
